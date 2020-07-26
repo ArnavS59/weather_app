7c47fb303b1b4163398dec904058eb7e5e763566
Arnav Singh <arnavbhaiya@gmail.com>
2020-07-26 23:11:12 +0200

    First commit

---
+++ lib/weather.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Weather extends StatefulWidget {
+  Weather({Key key}) : super(key: key);
+
+  @override
+  _WeatherState createState() => _WeatherState();
+}
+
+class _WeatherState extends State<Weather> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: null,
+    );
+  }
+}
